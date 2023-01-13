853846beba281340be9ce9f49e1a1cc3c0536d01
ohmsirapob <fedfekfc@gmail.com>
2023-01-13 17:17:28 +0700

    2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -12,12 +12,43 @@ void showImage(const bool [][M]);
 int main()
 {
     bool image[N][M] = {};
-    int s,x,y;
+    int s,x,y; 
     do{
         showImage(image);
         cout << "Input your brush size and location: ";
-        cin >> s >> x >> y;
+        cin >> s >> x >> y; 
         updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void showImage(const bool x[][M]){
+    for(int i=0 ;i < 72;i++) 
+    cout << "-"; 
+    cout << "\n";
+    
+    for(int i=0 ;i < 30;i++){  
+        cout <<"|";
+        for(int j=0;j < 70;j++){
+            if(x[i][j] == 0) cout << " "; 
+            else cout <<"*"; 
+        }
+        cout << "|"<<endl; 
+    }
+
+
+    for(int i=0 ;i <72;i++) 
+    cout << "-";
+    cout << "\n";
+    
+}
+
+void updateImage(bool aray[][M],int s,int x,int y){
+    for(int i=0;i < 30;i++){  
+        for(int j=0;j < 70;j++){ 
+            if(sqrt(pow(i-x,2)+pow(j-y,2))<= s-1){ 
+                aray[i][j] = 10;
+            }
+        }
+    }
 }
